26a54f666521a4adc958a9857ca1cb9c44f5a955
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-05-12 22:03:43 +0200

    funkce getList funguje - gerenuje seznam receptů - víc to zatím neumí

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
-/*
-Co je za úkol v tomto projektu:
+const detailReceptDiv = document.querySelector('.recept-detail');
+const detailImg = document.querySelector('#recept-foto');
+const detailTitle = document.querySelector('#recept-nazev');
+const detailDescr = document.querySelector('#recept-popis');
+const metaKategory = document.querySelector('#recept-kategorie');
+const metaRating = document.querySelector('#recept-hodnoceni');
 
-1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
-HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
 
-2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
-by se měl seznam receptů vyfiltrovat podle hledaného slova.
 
-3) Doplň filtrovanání receptů podle kategorie.
 
-4) Doplň řazení receptů podle hodnocení.
+//1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
+getList();
 
-5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
-Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
-recept-hodnoceni, recept-nazev, recept-popis.
+function getList() {
+    
+    for (let i = 0; i < recepty.length; i++) {
+        let singleRecept = document.createElement('div');
+        singleRecept.setAttribute ("class", "recept");
+        singleRecept.onclick = clickOnRecept;
 
-6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
-*/
+        let thumbnail = document.createElement('div');
+        thumbnail.setAttribute ("class", "recept-obrazek");
+
+        let thumbnailImg = document.createElement('img');
+        thumbnailImg.src = recepty[i].img;
+
+        let listTitle = document.createElement('div');
+        listTitle.setAttribute ("class", "recept-info");
+
+        let listTitleText = document.createElement('h3');
+        listTitleText.innerHTML = recepty[i].nadpis;
+
+        listTitle.appendChild(listTitleText);
+        thumbnail.appendChild(thumbnailImg);
+
+        singleRecept.appendChild(thumbnail);
+        singleRecept.appendChild(listTitle);
+
+        document.querySelector('.recepty').appendChild(singleRecept);
+    }
+}
+
+
+
+
+//2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat by se měl seznam receptů vyfiltrovat podle hledaného slova.
+
+//3) Doplň filtrovanání receptů podle kategorie.
+
+//4) Doplň řazení receptů podle hodnocení.
+
+//5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu. Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie, recept-hodnoceni, recept-nazev, recept-popis.
+function clickOnRecept() {
+
+}
+
+//6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
